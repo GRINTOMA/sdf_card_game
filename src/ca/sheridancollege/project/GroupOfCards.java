b35224b1faa29acf2a8bc0b0ef08696e36f2c4b8
Richard <doricha@sheridanc.on.ca>
2020-04-10 14:11:07 -0400

    Updated GroupOfCards with base functionality

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -9,10 +9,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- * A concrete class that represents any grouping of cards for a Game.
- * HINT, you might want to subclass this more than once.
- * The group of cards has a maximum size attribute which is flexible for reuse.
- * @author dancye
+ * An ArrayList of cards for tracking both the
+ * player's hand alongside the deck and pile 
+ * for the game.
  */
 public class GroupOfCards 
 {
@@ -21,37 +20,46 @@ public class GroupOfCards
     private ArrayList <Card> cards;
     private int size;//the size of the grouping
     
-    public GroupOfCards(int givenSize)
+    // default constructor
+    public GroupOfCards()
     {
-        size = givenSize;
+    }
+    
+    public void setCards(ArrayList<Card> cards){
+        this.cards = cards;
     }
     
     /**
      * A method that will get the group of cards as an ArrayList
      * @return the group of cards.
      */
-    public ArrayList<Card> showCards()
+    public ArrayList<Card> getCards()
     {
         return cards;
     }
     
+    // for shuffling the deck of cards
     public void shuffle()
     {
         Collections.shuffle(cards);
     }
-
-    /**
-     * @return the size of the group of cards
-     */
-    public int getSize() {
-        return size;
-    }
-
-    /**
-     * @param givenSize the max size for the group of cards
-     */
-    public void setSize(int givenSize) {
-        size = givenSize;
+    
+    // generates the deck of cards
+    // numOfDecks is determined by the number of players
+    // where passing a certain thresholds will
+    // involve multiple decks in the game
+    public void generateDeck(int numOfDecks){
+        for (Rank r : Rank.values()){
+            for (Suit s : Suit.values()){
+                // will add in duplicates of the card if necessary
+                for (int i = 0; i < numOfDecks; i++)
+                    cards.add(new Card(r, s));
+            }
+        }
     }
     
+    // add card to arraylist cards
+    public void addCard(Card card){
+        cards.add(card);
+    }
 }//end class
