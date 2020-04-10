1da3ca40f1678d20bb834a21d823ec7e8108fac6
Richard <doricha@sheridanc.on.ca>
2020-04-10 18:57:43 -0400

    Adjusted values for easier string comparison

--- src/ca/sheridancollege/project/Suit.java
+++ src/ca/sheridancollege/project/Suit.java
@@ -4,10 +4,10 @@ package ca.sheridancollege.project;
  * Represents the Suits for the Card class.
  */
 public enum Suit {
-    CLUBS(1, "Clubs", '\u2663'),
-    DIAMONDS(2, "Diamonds", '\u2662'),
-    HEARTS(3, "Hearts", '\u2661'),
-    SPADES(4, "Spades", '\u2660');
+    CLUBS(1, "C", '\u2663'),
+    DIAMONDS(2, "D", '\u2662'),
+    HEARTS(3, "H", '\u2661'),
+    SPADES(4, "S", '\u2660');
     
     private int value;
     private String name;
