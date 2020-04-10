a26d2f214ecd15fe279f039c0749b4b1897a9b9e
Richard <doricha@sheridanc.on.ca>
2020-04-10 12:57:20 -0400

    Updated Suit

--- src/ca/sheridancollege/project/Suit.java
+++ src/ca/sheridancollege/project/Suit.java
@@ -5,8 +5,33 @@ package ca.sheridancollege.project;
  * @author Richard
  */
 public enum Suit {
-    HEART,
-    DIAMOND,
-    SPADE,
-    CLUB
+    CLUBS(1, "Clubs", '\u2663'),
+    DIAMONDS(2, "Diamonds", '\u2662'),
+    HEARTS(3, "Hearts", '\u2661'),
+    SPADES(4, "Spades", '\u2660');
+    
+    private int value;
+    private String name;
+    private char symbol;
+    
+    private Suit(int value, String name, char symbol){
+        this.value = value;
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    // gets the value of each suit
+    public int getValue() {
+        return value;
+    }
+
+    // returns the name of each suit
+    public String getName() {
+        return name;
+    }
+    
+    // returns the symbol associated with the suit
+    public char getSymbol(){
+        return symbol;
+    }
 }
