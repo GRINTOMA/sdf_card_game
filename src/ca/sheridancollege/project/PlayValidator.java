88003092ea2059f74cdca15ad0580e7a095e4762
Richard <doricha@sheridanc.on.ca>
2020-04-11 10:48:48 -0400

    Updated to handle input for cards and cross referencing to hand

--- src/ca/sheridancollege/project/PlayValidator.java
+++ src/ca/sheridancollege/project/PlayValidator.java
@@ -1,10 +1,23 @@
 package ca.sheridancollege.project;
 
 /**
- *
- * @author Richard
+ * Checks for valid inputs and plays according
+ * to the game state and the current player's hand.
  */
+
 public class PlayValidator {
+    
+    // checks to see if the user input a valid card
+    public static boolean validateCard(String card){
+        return card.matches("[schdSCHD]([ajqkAJQK]|[1-9]|10)");
+    }
+    
+    // checks to see if user hand contains the card
+    // that they specified
+    public static boolean inHand(GroupOfCards hand, Card card){
+        return hand.getCards().contains(card);
+    }
+    
     // A valid play is one where either the played card matches
     // the suit of the top card or is an eight.
     public static boolean validatePlay(Card topCard, Card playCard){
